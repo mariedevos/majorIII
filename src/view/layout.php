846cc230578c816aa71ff74e3559b5507df6c73c
marie devos <mariedevos6@hotmail.com>
2019-01-22 22:12:37 +0100

    responsive header ok

--- src/view/layout.php
+++ src/view/layout.php
@@ -3,51 +3,46 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script>WebFont.load({
+
+  <script>
+  WebFont.load({
     custom: {
       families: ["baksosapi", "alvania"],
       urls: ["assets/font/fonts.css"]
     }
-  });</script>
+  });
+  </script>
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>major III</title>
-    <?php echo $css;?>
-  </head>
-  <body>
+  <title>major III</title>
+  <?php echo $css;?>
+</head>
+<body>
   <header>
-  <aside>nog 70 nachtjes slapen tot het intertnationaal straatheater festival</aside>
-  <h1>
-  <a href="index.php?page=home">.</a>
-  <p class="hidden">internationaal straattheater festival Beveren</p>
-  </h1>
-  <nav>
-  <ul>
-  <li><a href="index.php?page=programma">programma</a></li>
-  <li><a href="index.php?page=activiteiten">activiteiten</a></li>
-  <li><a href="index.php?page=praktisch">praktische info</a></li>
-  </ul>
-  </nav>
-  </header>
+    <aside>nog 70 nachtjes slapen tot het intertnationaal straatheater festival</aside>
+    <h1><a href="index.php?page=home">.</a><p class="hidden">internationaal straattheater festival Beveren</p></h1>
+    <nav>
+      <ul>
+        <li><a href="index.php?page=programma">programma</a></li>
+        <li><a href="index.php?page=activiteiten">activiteiten</a></li>
+        <li><a href="index.php?page=praktisch">praktische info</a></li>
+      </ul>
+    </nav>
+    </header>
+
+    <?php echo $content;?>
+    <?php echo $js; ?>
 
 
-	        <?php echo $content;?>
-      <?php echo $js; ?>
 
-
-      <footer>
-      <h1>
-  <a href="index.php?page=home">.</a>
-  <p class="hidden">internationaal straattheater festival Beveren</p>
-  </h1>
-
-
-  <h3>contact</h3>
-  <div class="contact">
-  <p>Dienst projectcoördinatie</p>
-  <p>03 750 16 29</p>
-  <p>erik.apers@beveren.be</p>
-  <p>Stationstraat 2 - 9120 Beveren</p>
-  </div>
+    <footer>
+      <h1><a href="index.php?page=home">.</a><p class="hidden">internationaal straattheater festival Beveren</p></h1>
+      <h3>contact</h3>
+      <div class="contact">
+        <p>Dienst projectcoördinatie</p>
+        <p>03 750 16 29</p>
+        <p>erik.apers@beveren.be</p>
+        <p>Stationstraat 2 - 9120 Beveren</p>
+      </div>
 
    <h3>volg ons</h3>
    <a href="">
@@ -57,7 +52,6 @@
    <img src="" alt="">instagram
    </a>
 
-
    <h3>praktische info</h3>
    <div class="contact">
    <a href="">toegankelijkheid</a>
@@ -65,9 +59,6 @@
    <a href="">activiteiten</a>
    <a href="">praktische info</a>
    </div>
-
-
-
-      </footer>
-      </body>
+  </footer>
+</body>
 </html>
