8df3481d1f034924c91e63f56fc9362b3dd1378b
marie devos <mariedevos6@hotmail.com>
2019-01-23 13:49:50 +0100

    fotos laden in

--- src/view/act/programma.php
+++ src/view/act/programma.php
@@ -2,8 +2,9 @@
   foreach($acts as $act){
     ?>
     <li class='act'>
+      <img src="assets/img/<?php echo $act['foto'];?>.jpg" alt="" width="200px">
         <p><?php echo $act['artiest'];?></p>
-        <><?php echo $act['titel'];?></p>
+        <?php echo $act['titel'];?></p>
         <a href="index.php?page=detail&amp;id=<?php echo $act['id']; ?>">detail</a>
 
     </li>
